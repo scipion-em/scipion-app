55e4ec132a6711efea909c7ad6ad6a45354e4c45
Yunior C. Fonseca Reyna <fonsecareyna82@gmail.com>
2019-09-26 16:44:56 +0200

    * More about the migration to python3

--- scipion/install/upgrade_release.py
+++ scipion/install/upgrade_release.py
@@ -20,27 +20,27 @@ EMPACK_DIRS_CONVERT = [
     {RELEASE_END:'unblur-1.0.15', RELEASE_ORI: 'unblur_1.0_150529'}
 ]
 
-print "This script is going to upgrade EM packages names from v1.0.1 to v1.1.0"
+print("This script is going to upgrade EM packages names from v1.0.1 to v1.1.0")
 
 # Script folder
 scd = os.path.dirname(__file__)
 # Software EM folder
 semd = os.path.join(scd, '..', 'software', 'em')
 
-print "Script folder %s" %  scd
-print "SEM folder %s" % semd
+print("Script folder %s" %  scd)
+print("SEM folder %s" % semd)
 
 # Move to Software EM folder
 os.chdir(semd)
 
 for emp in EMPACK_DIRS_CONVERT:
-    print "Check if %s exists" % emp[RELEASE_ORI]
+    print("Check if %s exists" % emp[RELEASE_ORI])
     if os.path.exists(emp[RELEASE_ORI]):
-        print "Create symlink between %s and %s" % (emp[RELEASE_END], emp[RELEASE_ORI])
+        print("Create symlink between %s and %s" % (emp[RELEASE_END], emp[RELEASE_ORI]))
         os.symlink(emp[RELEASE_ORI], emp[RELEASE_END])
 
 # Move to back Scipion home
 os.chdir("../..")
 
-print "EM packages installed after the upgrade"
+print("EM packages installed after the upgrade")
 os.system('./scipion install --help')

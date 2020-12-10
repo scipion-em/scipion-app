9ed8498cdb5f348bc4ee4692685b7b394c171e18
pconesa <p.conesa.mingo@gmail.com>
2020-12-10 13:37:22 +0100

    adding 100 protocols

--- scipion/scripts/profiling-load.py
+++ scipion/scripts/profiling-load.py
@@ -8,6 +8,8 @@ from scipion.__main__ import main
 main(justinit=True)
 
 import pyworkflow
+from  pyworkflow.object import Pointer
+from pyworkflowtests.protocols import ProtOutputTest
 from pyworkflow.project import Manager, Project
 
 # Create a new project
@@ -20,7 +22,27 @@ if len(sys.argv)>1:
 elif len(sys.argv) == 1:
     projectFolder = tempfile.mkdtemp()
     print("Project folder at: %s" % projectFolder)
-    manager.createProject(os.path.basename(projectFolder), location=os.path.dirname(projectFolder))
+    project = manager.createProject(os.path.basename(projectFolder), location=os.path.dirname(projectFolder))
+    
+    previousProt = None
+    currentProt = None
+    # Add protocols
+    for count in range(100):
+        args = dict()
+
+        newProt = project.newProtocol(ProtOutputTest, iBoxSize=10)
+
+        # if previousProt is not None:
+            # newProt.iBoxSize.setPointer(Pointer(previousProt, extended="oBoxSize"))
+
+        if currentProt is not None:
+            previousProt = currentProt
+
+        currentProt = newProt
+
+        currentProt._store()
+    
+
 
 project = Project(pyworkflow.Config.getDomain(), projectFolder)
 project.load()
